253125b87c230420a966e55e4a2f8d835ef37601
Kristian <popovichkristian@gmail.com>
2022-08-29 18:16:48 +0300

    CodeWars Kata #5

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ function between(a, b) {
   }
   
   console.log(between(1, 5));
+
+  var humanYearsCatYearsDogYears = function(humanYears) {
+    let cy = 0;
+    let dy = 0;
+    let arr = [];
+    
+    if (humanYears === 1) {
+      cy = 15;
+      dy = 15;
+    } else if (humanYears === 2) {
+      cy = 24;
+      dy = 24;
+    } else if (humanYears > 2) {
+      cy = 24 + 4 * (humanYears - 2);
+      dy = 24 + 5 * (humanYears - 2);
+    }
+    
+    arr.push(humanYears, cy, dy);
+    return arr;
+  }
